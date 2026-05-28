3d409c4fc2f1cc2a395d70a34c8c471218f3a968
Lic.Robinson Herrera Diaz <100285607rh@gmail.com>
2026-05-28 01:47:43 -0400

    listo

---
+++ gitPrueva02/Controllers/robinson.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gitPrueva02.Controllers
+{
+    public class robinson : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
